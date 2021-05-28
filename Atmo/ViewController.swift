4bfa5af629152b7855eee8e904e2c3b4b3f13e2a
Mark Philips <mphilips15@gmail.com>
2021-05-28 13:27:23 -0400

    Initial Commit

---
+++ Atmo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Atmo
+//
+//  Created by Mark Philips on 2021-05-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
