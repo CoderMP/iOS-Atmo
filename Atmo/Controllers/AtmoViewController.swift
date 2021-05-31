fe2049130f9b1de80129228176fff1a2075bbeaa
Mark Philips <mphilips15@gmail.com>
2021-05-31 12:15:32 -0400

    Added CLLocationManagerDelegate extension and related methods to AtmoViewController

--- Atmo/Controllers/AtmoViewController.swift
+++ Atmo/Controllers/AtmoViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class AtmoViewController: UIViewController {
     @IBOutlet var searchTextField: UITextField!
@@ -17,10 +18,20 @@ class AtmoViewController: UIViewController {
     @IBOutlet var feelsLikeLabel: UILabel!
     @IBOutlet var humidityLabel: UILabel!
     
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // TODO: CoreLocation and AtmoWeatherManager delegate initialization
+        // Set the CoreLocation's delegate
+        locationManager.delegate = self
+        
+        // Request the user's location
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
+        // Set the searchTextField & AtmoWeatherManager delegates
+        searchTextField.delegate = self
     }
 }
 
@@ -90,5 +101,60 @@ extension AtmoViewController: UITextFieldDelegate {
         
         // Clear the text field
         searchTextField.text = ""
+    }
+}
+
+//MARK: - CLLocationManagerDelegate
+extension AtmoViewController: CLLocationManagerDelegate {
+    @IBAction func locationPressed(_ sender: UIButton) {
+        // Check to see whether or not the LocationManager's location is nil
+        if (locationManager.location != nil) {
+            // request the location
+            locationManager.requestLocation()
+            
+            // Visualize the use of location services being used to the user via animating the location button
+            UIView.transition(with: locationButton!, duration: 0.5, options: .transitionFlipFromRight, animations: {
+                self.locationButton.setImage(UIImage(systemName: "location.fill"), for: .normal)
+            }, completion: nil)
+        }
+    }
+    
+    /*
+     * Delegate function that is responsible for handling execution logic based on updated locations from
+     * the CLLocationManager
+     *
+     * @param manager The CLLocationManager that will use the delegate function
+     * @param locations The array of locations captured via the CLLocationManager's requestLocation()
+     *
+     * @author Mark Philips
+     */
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            // Stop the LocationManager from getting a new location
+            locationManager.stopUpdatingLocation()
+            
+            // Extract the latitiude and longitude from the most accurate location entry
+            let lat = location.coordinate.latitude
+            let long = location.coordinate.longitude
+            
+            // TODO: Fetch the weather conditions for the current location
+            
+            if let locationFill = UIImage(systemName: "location.fill") {
+                locationButton.setImage(locationFill, for: .normal)
+            }
+        }
+    }
+    
+    /*
+     * Delegate function that is responsible for handling any errors that occur with the LocationManager
+     *
+     * @param manager The CLLocationManager that will use the delegate function
+     * @param error The error that occurred
+     *
+     * @author Mark Philips
+     */
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        // Print the error to the console
+        print(error)
     }
 }
