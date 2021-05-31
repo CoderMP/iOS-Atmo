6c92592ccb844fdc4ef7122ad23fa9cd1fe6efd8
Mark Philips <mphilips15@gmail.com>
2021-05-31 11:16:58 -0400

    Connected UI components in Storyboard to AtmoViewController

--- Atmo/Controllers/AtmoViewController.swift
+++ Atmo/Controllers/AtmoViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class AtmoViewController: UIViewController {
+    @IBOutlet var searchTextField: UITextField!
+    @IBOutlet var locationButton: UIButton!
+    @IBOutlet var weatherConditionImage: UIImageView!
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var cityNameLabel: UILabel!
+    @IBOutlet var weatherConditionLabel: UILabel!
+    @IBOutlet var feelsLikeLabel: UILabel!
+    @IBOutlet var humidityLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
